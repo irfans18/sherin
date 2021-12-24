2d685f0889efd66d42f9c22c2d37575d55c1471b
@yopigambyok <yopigambyok@gmail.com>
2021-12-24 16:22:03 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/peserta', function () {
+    return view('peserta');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
