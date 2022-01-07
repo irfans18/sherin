aa7eb2131c52699f5ca05165431503e4ca80be8d
Khoiron14 <muh.khoiron14@gmail.com>
2022-01-07 20:44:48 +0700

    fix list peserta 5

--- resources/views/list-peserta.blade.php
+++ resources/views/list-peserta.blade.php
@@ -67,7 +67,7 @@
                                                 {{ $row->email }}
                                             </td>
                                             <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
-                                                {{ $row->groupMember->count() }}
+                                                {{ $row->groupMember ? 'ada' : 'tidak' }}
                                             </td>
                                             <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
                                                 {{ $row->requests->count() }}
